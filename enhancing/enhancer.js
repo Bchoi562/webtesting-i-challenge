e9c636306167698d1d45b971cfce11cb02043ef6
Brian Choi <b.choi562@gmail.com>
2021-02-24 17:33:38 -0800

    MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,27 @@
-module.exports = {
-  success,
-  fail,
-  repair,
-  get,
+
+const success = (item) => {
+  if (item.enhancement < 20) {
+      return { ...item, enhancement: item.enhancement + 1 };
+  } else {
+      return { ...item };
+  }
 };
 
-function success(item) {
-  return { ...item };
-}
+const fail = (item) => {
+  if (item.enhancement <= 15) {
+      return { ...item, durability: item.durability - 5 };
+  } else {
+      return { ...item, 
+              durability: item.durability - 10,
+              enhancement: item.enhancement - 1 };
+  }
+};
 
-function fail(item) {
-  return { ...item };
-}
-
-function repair(item) {
-  return { ...item };
-}
-
-function get(item) {
-  return { ...item };
-}
+const repair = (item) => {
+  return { ...item, durability: 100 };
+};
+module.exports = {
+success,
+fail,
+repair,
+};
